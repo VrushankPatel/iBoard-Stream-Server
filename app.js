59b96c555b66c77dfc1144db29dcc2a26829bda1
Vrushank Patel <vrushankpatel5@gmail.com>
2021-07-16 10:41:23 +0530

    added cors

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const http = require("http");
 const socketIo = require("socket.io");
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
+var cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(index);
 
 const server = http.createServer(app);
