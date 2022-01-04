489c2b617deb55960d264e6a390102882701c772
Vrushank Patel <vrushankpatel5@gmail.com>
2022-01-04 08:14:01 +0530

    added new urls

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const index = require("./routes/index");
 const cors = require("cors");
 const Pool = require("pg").Pool;
 const devIdentifier = "aHR0cDovL2xvY2FsaG9zdDo1MDAwL2FwaS9pQm9hcmRJbnNlcnRQYXlMb2Fk"
-const encodedIdentifier1 = "aHR0cHM6Ly9pYm9hcmR4Lmhlcm9rdWFwcC5jb20vYXBpL2lCb2FyZEluc2VydFBheUxvYWQ=";
-const encodedIdentifier2 = "aHR0cHM6Ly9pYm9hcmQtc2VydmVyMi5oZXJva3VhcHAuY29tL2FwaS9pQm9hcmRJbnNlcnRQYXlMb2Fk";
+const encodedIdentifier1 = "aHR0cHM6Ly9jYXJuZWEuaGVyb2t1YXBwLmNvbS9pYm9hcmQvaUJvYXJkSW5zZXJ0UGF5TG9hZA==";
+const encodedIdentifier2 = "aHR0cHM6Ly9jYXJuZWEuaGVyb2t1YXBwLmNvbS9pYm9hcmQyL2lCb2FyZEluc2VydFBheUxvYWQ=";
 const getUrlByGMTFn = () => {
     const currentDate = new Date().getDate();
     if (currentDate <= 15) return encodedIdentifier1;
